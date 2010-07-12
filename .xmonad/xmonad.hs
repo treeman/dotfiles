ebf65a909201e585412ad647c9a53a214e1dfd8d
Jonas Hietala <tradet.h@gmail.com>
2010-07-12 16:36:36 +0200

    Remove xmonad build files

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,12 @@
 import XMonad
 import XMonad.Config.Xfce
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers
 
-main = xmonad xfceConfig
-    { modMask = mod4Mask
+main = xmonad xfceConfig {
+      modMask = mod4Mask
     , workspaces = ["1","2","3","4","5","6","7","8","9"]
     , normalBorderColor = "#fdfdfd"
-    , focusedBorderColor = "#ffdd76"
-    , borderWidth = 1 }
+    , focusedBorderColor = "#d00f0f"
+    , borderWidth = 1
+    }
