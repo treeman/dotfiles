34ac13e9094b119091056f8aaaaa29dbf2d25865
Jonas Hietala <tradet.h@gmail.com>
2010-08-01 15:29:39 +0200

    Sorted history loading with better hotkey

--- .uzbl/scripts/load_url_from_history.sh
+++ .uzbl/scripts/load_url_from_history.sh
@@ -5,6 +5,6 @@ file=$HOME/.uzbl/data/history
 
 COLORS=" -fn monospace -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
 DMENU="dmenu -i -b -l 10"
-goto=`sort -k3 < $file | uniq -f2 | awk '{print $3}' | $DMENU $COLORS`
+goto=`sort -k3 < $file | uniq -f2 | sort -r | awk '{print $3}' | $DMENU $COLORS`
 
 [ -n "$goto" ] && echo "uri $goto" > $4
