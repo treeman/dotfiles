1675bb310111b6ebfe664934865ebd914aaca3ea
Jonas Hietala <tradet.h@gmail.com>
2010-08-01 17:50:02 +0200

    Tab open history and bookmarks

--- .uzbl/scripts/load_url_from_bookmarks.sh
+++ .uzbl/scripts/load_url_from_bookmarks.sh
@@ -3,8 +3,10 @@
 file=$HOME/.uzbl/data/bookmarks
 [ -r "$file" ] || exit
 
+action=$8
+
 COLORS=" -fn monospace -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
 DMENU="dmenu -i -b -l 10"
 goto=`awk '{print $3}' $file | $DMENU $COLORS`
 
-[ -n "$goto" ] && echo "uri $goto" > $4
+[ -n "$goto" ] && echo "$action $goto" > $4
