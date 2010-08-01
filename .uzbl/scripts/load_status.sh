f49ba28e37549eaa50615d46274869091e94e87e
Jonas Hietala <tradet.h@gmail.com>
2010-08-01 14:57:47 +0200

    Fixed status loading

--- .uzbl/scripts/load_status.sh
+++ .uzbl/scripts/load_status.sh
@@ -7,11 +7,14 @@ if [ -r $file ]; then
     show_tablist=$(echo `cat $file` |cut -d',' -f1)
     show_status=$(echo `cat $file` |cut -d',' -f2)
 else
-    show_tablist=1;
-    show_status=1;
+    show_tablist=1
+    show_status=1
 
-    echo "1,1" > $file;
-fi;
+    echo "1,1" > $file
+fi
 
-echo "set show_tablist = $show_tablist" > $fifo;
-echo "set show_status = $show_status" > $fifo;
+#echo "set show_tablist = $show_tablist" > $fifo
+#echo "set show_status = $show_status" > $fifo
+
+echo "set show_tablist = 0" > $fifo
+echo "set show_status = 0" > $fifo
