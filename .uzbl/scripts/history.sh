07d53fb0a232aa4f4083258ffbee32f9a55b4c3b
Jonas Hietala <tradet.h@gmail.com>
2010-08-10 20:48:58 +0200

    Don't add non-urls to history

--- .uzbl/scripts/history.sh
+++ .uzbl/scripts/history.sh
@@ -2,4 +2,7 @@
 
 file=/home/tree/.uzbl/data/history
 [ -d `dirname $file` ] || exit 1
-echo `date +'%Y-%m-%d %H:%M:%S'`" $6 $7" >> $file
+
+if [ -n "$6" ]; then
+    echo `date +'%Y-%m-%d %H:%M:%S'`" $6 $7" >> $file
+fi
