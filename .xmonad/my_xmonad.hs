3b52a1be67467369046315d7efcf633b36f77860
Jonas Hietala <tradet.h@gmail.com>
2010-08-25 23:00:31 +0200

    Xmonad handles keybindings and bind for toggling window borders.

---
+++ .xmonad/my_xmonad.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
+
+import XMonad
+import XMonad.Actions.NoBorders
+import XMonad.Actions.WithAll
+import XMonad.Config.Xfce
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers
+
+import qualified Data.Map as M
+
+myKeys (XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm,                   xK_w), spawn "uzbl")
+    , ((modm,                   xK_m), spawn "wine \"C:/Program files/spotify/spotify.exe\"")
+    , ((modm,                   xK_p), spawn "pidgin")
+    , ((modm,                   xK_f), spawn "firefox")
+    , ((modm,                   xK_s), spawn "skype")
+    , ((modm,                   xK_i), spawn "xterm -e irssi")
+    , ((modm .|. shiftMask,     xK_b), withAll toggleBorder)
+    ]
+
+main = xmonad xfceConfig {
+      modMask = mod4Mask
+    , workspaces = ["1","2","3","4","5","6","7","8","9"]
+    , normalBorderColor = "#fdfdfd"
+    , focusedBorderColor = "#d00f0f"
+    , borderWidth = 1
+    , terminal = "xterm"
+    , keys = \k -> myKeys k `M.union` keys xfceConfig k
+}
+
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,1 @@
-import XMonad
-import XMonad.Config.Xfce
-import XMonad.Layout.NoBorders
-import XMonad.Hooks.ManageHelpers
-
-main = xmonad xfceConfig {
-      modMask = mod4Mask
-    , workspaces = ["1","2","3","4","5","6","7","8","9"]
-    , normalBorderColor = "#fdfdfd"
-    , focusedBorderColor = "#d00f0f"
-    , borderWidth = 0
-    }
+my_xmonad.hs
