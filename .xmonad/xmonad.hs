33af31adcafec13d1f178d851183c06df6b1f5a4
Jonas Hietala <tradet.h@gmail.com>
2010-09-17 11:17:02 +0200

    Adding

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,1 +1,12 @@
-my_xmonad.hs
+import XMonad
+import XMonad.Config.Xfce
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers
+
+main = xmonad xfceConfig {
+      modMask = mod4Mask
+    , workspaces = ["1","2","3","4","5","6","7","8","9"]
+    , normalBorderColor = "#fdfdfd"
+    , focusedBorderColor = "#d00f0f"
+    , borderWidth = 0
+    }
