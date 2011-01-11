3de32a6c1e979bc86fdb72cbb7ae4ab3f65c485f
Jonas Hietala <tradet.h@gmail.com>
2011-01-11 16:36:54 +0100

    More xmonad shortcuts.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,11 +11,15 @@ import qualified Data.Map as M
 
 myKeys (XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm,                   xK_w), spawn "uzbl")
-    , ((modm,                   xK_m), spawn "wine \"C:/Program files/spotify/spotify.exe\"")
+    , ((modm,                   xK_m), spawn "spotify")
     , ((modm,                   xK_p), spawn "pidgin")
     , ((modm,                   xK_f), spawn "firefox")
     , ((modm,                   xK_s), spawn "skype")
     , ((modm,                   xK_i), spawn "xterm -e irssi")
+    , ((modm,                   xK_c), spawn "google-chrome")
+    , ((modm,                   xK_t), spawn "mtpaint")
+    --, ((modm,                   xK_h), spawn "Thunar")
+    , ((modm .|. shiftMask,     xK_p), spawn "scrot screenshots/screen_%Y-%m-%d_%T.jpg -d")
     , ((modm .|. shiftMask,     xK_b), withAll toggleBorder)
     ]
 
