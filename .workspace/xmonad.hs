2afbd5734a4ed656f46f709c58d07c67e78ffae7
Jonas Hietala <tradet.h@gmail.com>
2011-08-20 15:51:11 +0200

    Coloring statusbar. Icons shows what layout and useful upper right side.

--- .workspace/xmonad.hs
+++ .workspace/xmonad.hs
@@ -55,6 +55,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
     ]
 
+-- Delete later, not used
 myLogHook h = dynamicLogWithPP $ xmobarPP
   { ppOutput          = hPutStrLn h
   , ppTitle           = color "grey" . shorten 50
@@ -67,13 +68,10 @@ myLogHook h = dynamicLogWithPP $ xmobarPP
   where color x = xmobarColor x ""
         dropId x = if (':' `elem` x) then drop 2 x else x
 
+--["α","β","γ","δ","ε","ζ"]
 myWorkspaces = ["α","β","γ", "4:prog", "5:www", "6:chat", "7:irc", "8:music", "9:misc"]
 
---myFont = "-*-montecarlo-medium-r-normal-*-11-*-*-*-c-*-*-*"
---myFont = "-fn -*-terminus-*-*-*-*-10-*-*-*-*-*-iso8859"
---myFont = "-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*"
-myFont = "Consolasi:size=9"
-myIconDir = "/home/and1/.dzen"
+--myIconDir = "/home/and1/.dzen"
 myDzenFGColor = "#555555"
 myDzenBGColor = "#222222"
 myNormalFGColor = "#ffffff"
@@ -86,35 +84,62 @@ myIconFGColor = "#777777"
 myIconBGColor = "#0f0f0f"
 mySeperatorColor = "#555555"
 
-myDzen = " dzen2 -xs 1 -dock -h 18 -ta 'l' -fn '" ++ myFont ++ "' -fg '" ++ myNormalFGColor ++ "' -bg '" ++ myNormalBGColor ++ "' "
---myDzen = " dzen2 -dock -h 18 -ta 'l' -fn '" ++ myFont ++ "' -fg '" ++ myNormalFGColor ++ "' -bg '" ++ myNormalBGColor ++ "' "
+myFont = "Consolasi:size=9"
+myIconDir = "/home/tree/.workspace/icons/"
+myNormalStatusFG = "#616161"
+myNormalStatusBG = "#0f0f0f"
+
+-- Triades
+-- yellow: #F2FF00
+-- lightblue: #00F2FF
+-- pink: #FF00F2
+--
+-- Tetrades
+-- red: #BD5E5E
+-- green: #8EBD5E
+-- blue: #5EBDBD
+-- purple: #8E5EBD
+
+myDzen = " dzen2 -xs 1 -dock -h 18 -ta 'l' -fn '" ++ myFont ++ "' -fg '" ++ myNormalStatusFG ++ "' -bg '" ++ myNormalStatusBG ++ "' "
 
 myStatusBar = myDzen ++ " -x '0' -y '0' -ta 'l' -w 800"
+myTopRight = "conky -c ~/.workspace/conky_bar | " ++ myDzen ++ " -x '800' -y '0' -ta 'r' -p"
 
-myTopRight = "echo pew! | " ++ myDzen ++ " -x '800' -y '0' -ta 'r' -p"
+myHackerTop = "conky -c ~/.workspace/conkyrc-hackertop"
 
 myDzenPP h = defaultPP
-    { ppCurrent = wrapFg "#FFFF00" . dropId
-    , ppVisible = wrapFg "#F711DD" . dropId
-    , ppHidden = wrapFg "#1FA0E6" . dropId
-    , ppHiddenNoWindows = wrapFg "#ACACAC" . dropId
-    , ppOutput = hPutStrLn h
-    , ppTitle = wrap "< " " > "
+    --{ ppCurrent = wrapFg "#FFFF00" . dropId
+    --, ppVisible = wrapFg "#F711DD" . dropId
+    --, ppHidden = wrapFg "#1FA0E6" . dropId
+    --, ppHiddenNoWindows = wrapFg "#616161" . dropId
+    { ppOutput = hPutStrLn h
+    , ppCurrent = wrapFg "#8EBD5E" . dropId
+    , ppVisible = wrapFg "#8E5EBD" . dropId
+    , ppHidden = wrapFg "#5EBDBD" . dropId
+    , ppHiddenNoWindows = wrapFg "#616161" . dropId
+    , ppUrgent = wrapFg "#BD5E5E" . dropId
+    , ppTitle = wrap "< " " > " . wrapFg "#ffffff"
+    , ppSep = " "
+    , ppLayout = wrapFg myNormalStatusFG .
+        (\x -> case x of
+            "Tall" -> wrapBitmap "Tall.xbm"
+            "Mirror Tall" -> wrapBitmap "MirrorTall.xbm"
+            "Full" -> wrapBitmap "Full.xbm"
+            _ -> x
+        )
     }
     where dropId x = if (':' `elem` x) then drop 2 x else x
           wrapFgBg fg bg content = wrap ("^fg(" ++ fg ++ ")^bg(" ++ bg ++ ")") "^fg()^bg()" content
           wrapFg color content = wrap ("^fg(" ++ color ++ ")") "^fg()" content
           wrapBg color content = wrap ("^bg(" ++ color ++ ")") "^bg()" content
-          --wrapBitmap bitmap = "^p(5)^i(" ++ bitmap ++ ")^p(5)"
+          wrapBitmap bitmap = "^p(2)^i(" ++ myIconDir ++ bitmap ++ ")^p(2)"
 
 main = do
-    --xmproc <- spawnPipe "xmobar" -- force xmonad to avoid dzen, fugly hack
     topLeft <- spawnPipe myStatusBar
     topRight <- spawnPipe myTopRight
+    --hackertop <- spawnPipe myHackerTop
     xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
       modMask = mod4Mask
-    --, workspaces = map show [1..9]
-    --, workspaces = ["α","β","γ","δ","ε","ζ"] ++ map show [7..9]
     , workspaces = myWorkspaces
     , normalBorderColor = "#000000"
     , focusedBorderColor = "#363636"
