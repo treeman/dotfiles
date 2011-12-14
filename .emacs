9298c1feb7e2b9bb263f45a90a1ab9d3a523f31b
Jonas Hietala <tradet.h@gmail.com>
2011-12-14 08:54:25 +0100

    Add in emacs, nothing there but might be soon enough. Remove tons of crap from gitignore. Add in irssi scripts and theme, not the config though.

--- .emacs
+++ .emacs
@@ -1,19 +1,5 @@
-﻿(add-to-list 'load-path "~/.emacs.d/")
-(add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized/")
-
-; Some theme I'm testing
-;(require 'color-theme)
-;(color-theme-initialize)
-;(color-theme-billw)
-
-; Slightly smaller than regular
-(set-face-attribute 'default (selected-frame) :height 90)
-
-; Turn off annoying things
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-
-(blink-cursor-mode 0)
-(column-number-mode 1)
-(scroll-bar-mode 0)
-
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
---
+++ .irssi/scripts/autorun/beepchan.pl
@@ -0,0 +1,40 @@
+use strict;
+use vars qw($VERSION %IRSSI);
+use Irssi qw(signal_add signal_emit settings_add_str settings_get_str
+            settings_add_bool settings_get_bool);
+
+$VERSION = '0.10';
+%IRSSI = (
+    authors     => 'Jerith',
+    contact     => 'jerith@nightstar.net',
+    name        => 'beepchan',
+    description => 'Sends a beep signal when messages are received on '.
+                    'selected channels.',
+    license     => 'Public Domain',
+);
+
+sub beepchan {
+    my ($server, $msg, $nick, $address, $target) = @_;
+
+    return if not settings_get_bool('beep_on_channel');
+    my $channels = settings_get_str('beep_channels');
+    $channels =~ tr/ /|/;
+    Irssi::signal_emit('beep') if $target =~ /$channels/i;
+}
+
+sub beephilight {
+    my ($dest, $text, $stripped) = @_;
+
+    return if not settings_get_bool('beep_on_hilight');
+
+    Irssi::signal_emit('beep') if (($dest->{level} & (MSGLEVEL_HILIGHT|MSGLEVEL_MSGS))
+                                && ($dest->{level} & MSGLEVEL_NOHILIGHT) == 0);
+}
+
+Irssi::signal_add('message public', 'beepchan');
+Irssi::signal_add('message irc action', 'beepchan');
+Irssi::signal_add('print text', 'beephilight');
+Irssi::settings_add_bool('misc', 'beep_on_channel', 1);
+Irssi::settings_add_bool('misc', 'beep_on_hilight', 1);
+Irssi::settings_add_str('misc','beep_channels','#madeoftree #viperassassinsquad');
+
