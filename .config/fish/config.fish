f5e88a4b9dacee987ac693e414c943ae082e724c
Jonas Hietala <mail@jonashietala.se>
2015-07-30 08:50:18 +0200

    Include fonts.conf and some fish configs.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,17 @@
-function ll
-    ls -lh $argv
+
+# Some slackbuild scripts want this
+set -x ARCH x86_64
+set -x PATH ~/bin /opt/man-db/bin /opt/man-db/sbin $PATH
+
+alias vim nvim
+alias g git
+
+# List and sort packages by size
+function packages
+    fgrep UNCOMPRESSED /var/log/packages/* | awk -F: '{print $3, $1}' | sort -h
 end
+
+function reload
+    source ~/.config/fish/config.fish
+end
+
