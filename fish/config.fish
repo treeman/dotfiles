5ee9f07510861aae1b850a8159c6775d7fb6ebef
Jonas Hietala <mail@jonashietala.se>
2019-02-18 08:23:17 +0100

    Gruvbox fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,13 @@
-# Base16 Shell
-#eval sh $HOME/.config/base16-shell/base16-default.dark.sh
-
+# Paths
 set -x PATH $PATH /usr/local/sbin /usr/sbin /sbin
 set -x PATH ~/.cabal/bin $PATH
 set -x PATH ~/bin ~/dotfiles/bin $PATH
-
-# For java
-#set -x PATH $PATH /usr/lib64/java/bin
-
-# For rust...?
-#set -x LD_LIBRARY_PATH /usr/local/lib /usr/local/lib64 /lib /usr/lib /usr/lib64 $LD_LIBRARY_PATH
 
 # Workaround for local timezone discovery
 set -x TZ Europe/Stockholm
 
 # git message editor and other things
 set -x EDITOR nvim
-
-# enable true colors in nvim
-set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 
 alias vim nvim
 alias g git
@@ -28,3 +17,6 @@ set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 function reload
     source ~/.config/fish/config.fish
 end
+
+# Colorscheme
+theme_gruvbox dark hard
