f9f5e687bec5c39026c59c17f82ea28fd08548db
Jonas Hietala <mail@jonashietala.se>
2022-12-06 09:17:32 +0100

    Remove taskwarrior gtd fish greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,20 +1,3 @@
 function fish_greeting
-    if contains -- $USER root toor
-        return
-    end
-
-    set projects (projects_without_next)
-
-    if test "x-$projects" != "x-"
-        echo "Attention: The following projects don't currently have a next action:"
-        echo $projects
-        echo
-    end
-
-    set waiting (task +WAITING +PENDING count)
-    if test $waiting != "0"
-        echo "Any progress on these waiting-fors?"
-        task +WAITING +PENDING ls
-    end
 end
 
