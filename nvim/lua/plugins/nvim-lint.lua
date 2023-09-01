73fdf3055143e17eb239353501eb86146bcfd924
Jonas Hietala <mail@jonashietala.se>
2023-09-01 13:55:31 +0200

    Reformat

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,17 +1,17 @@
-function config()
-  require('lint').linters_by_ft = {
-    markdown = {'vale'}
-  }
+local function config()
+	require("lint").linters_by_ft = {
+		markdown = { "vale" },
+	}
 
-  vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
-    callback = function()
-      require("lint").try_lint()
-    end,
-  })
+	vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+		callback = function()
+			require("lint").try_lint()
+		end,
+	})
 end
 
 return {
-  "mfussenegger/nvim-lint",
-  config = config,
-  event = "BufReadPre",
+	"mfussenegger/nvim-lint",
+	config = config,
+	event = "BufReadPre",
 }
