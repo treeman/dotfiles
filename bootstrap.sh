13c8fddece123b967e084154a78dbec9ff091777
Jonas Hietala <mail@jonashietala.se>
2023-09-11 08:38:35 +0200

    Move dotfiles to their proper locations

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p ~/.config
+for dir in ~/dotfiles/.config/*
+do
+    echo $dir
+    ln -s $dir ~/.config/
+done
+
+ln -s ~/dotfiles/.conky ~/
+ln -s ~/dotfiles/.dir_colors ~/
+ln -s ~/dotfiles/.ghci ~/
+ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.screenrc ~/
+ln -s ~/dotfiles/.xmonad ~/
+ln -s ~/dotfiles/bin ~/
