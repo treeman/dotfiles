2cebbf6ce097c48905dce83783e719bde0492eec
Jonas Hietala <mail@jonashietala.se>
2023-09-15 20:32:32 +0200

    Update plugins

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -39,7 +39,40 @@ local location = "%c:%l/%L"
 
 -- Theme is hard.
 -- I dumped the "auto" theme and tried to adjust some things that bothered me a little.
-local colors = require("melange.palettes.dark")
+local colors = {
+	a = {
+		bg = "#292522",
+		float = "#34302C",
+		sel = "#403A36",
+		ui = "#867462",
+		com = "#C1A78E",
+		fg = "#ECE1D7",
+	},
+	b = {
+		red = "#D47766",
+		yellow = "#EBC06D",
+		green = "#85B695",
+		cyan = "#89B3B6",
+		blue = "#A3A9CE",
+		magenta = "#CF9BC2",
+	},
+	c = {
+		red = "#BD8183",
+		yellow = "#E49B5D",
+		green = "#78997A",
+		cyan = "#7B9695",
+		blue = "#7F91B2",
+		magenta = "#B380B0",
+	},
+	d = {
+		red = "#7D2A2F",
+		yellow = "#8B7449",
+		green = "#233524",
+		cyan = "#253333",
+		blue = "#273142",
+		magenta = "#422741",
+	},
+}
 local custom_theme = {
 	normal = {
 		a = { bg = "#463f3b", fg = "#96918e", gui = "bold" },
