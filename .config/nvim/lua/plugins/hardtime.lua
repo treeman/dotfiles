0e8f3537bb62b426ff9a9a9e58e4447a85cbf6eb
Jonas Hietala <mail@jonashietala.se>
2023-09-16 09:57:05 +0200

    Hardtime, a glutton for punishment

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,18 @@
+local opts = {
+	disabled_keys = {
+		-- up/down is aliased to gj and gk, and besides using the T-34 keyboard layout arrows aren't a problem.
+		["<Up>"] = {},
+		["<Down>"] = {},
+		["<Left>"] = {},
+		["<Right>"] = {},
+	},
+	disabled_filetypes = { "qf", "NvimTree", "lazy", "mason", "oil", "alpha", "neo-tree", "NeogitStatus" },
+	allow_different_key = true,
+}
+
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = opts,
+	lazy = false,
+}
