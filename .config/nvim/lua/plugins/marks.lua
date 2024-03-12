a196481ed10dbbbcb22d879c63819cdd753f1e4f
Jonas Hietala <mail@jonashietala.se>
2024-03-12 11:24:25 +0100

    Hide marks in sidebar

--- .config/nvim/lua/plugins/marks.lua
+++ .config/nvim/lua/plugins/marks.lua
@@ -2,7 +2,6 @@ return {
 	"chentoast/marks.nvim",
 	config = function()
 		require("marks").setup({
-			builtin_marks = { ".", "<", ">", "'" },
 			mappings = require("config.keymaps").marks,
 			default_mappings = false,
 		})
