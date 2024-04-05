bd169b308221903a13a5670a8df2810aa2f2cd24
Jonas Hietala <mail@jonashietala.se>
2024-04-05 20:46:01 +0200

    WIP rg titles

--- .config/nvim/lua/blog/telescope.lua
+++ .config/nvim/lua/blog/telescope.lua
@@ -5,55 +5,93 @@ local finders = require("telescope.finders")
 local pickers = require("telescope.pickers")
 local previewers = require("telescope.previewers")
 local server = require("blog/server")
+local nio = require("nio")
 
 local M = {}
 
+local blog_path = "/home/tree/code/jonashietala/"
+
 M.show_tags = function(opts)
-	print("show tags")
 	server.list_tags(function(_, res)
 		if res then
-			pickers
-				.new(opts, {
-					finder = finders.new_table({
-						results = res.tags,
-						entry_maker = function(entry)
-							return {
-								display = entry.name .. " (" .. tostring(#entry.posts) .. ")",
-								ordinal = entry.name,
-								value = entry,
-							}
-						end,
-					}),
-					-- TODO sort tags by count?
-					sorter = config.generic_sorter(opts),
-					previewer = previewers.new_buffer_previewer({
-						title = "Tag detaiLs",
-						define_preview = function(self, entry)
-							local lines = {}
-							for _, post in ipairs(entry.value.posts) do
-								table.insert(lines, post.title)
-								table.insert(lines, post.url)
-								table.insert(lines, "")
-							end
-							vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, lines)
-						end,
-					}),
-					attach_mappings = function(prompt_bufnr)
-						actions.select_default:replace(function()
-							local selection = action_state.get_selected_entry()
-							actions.close(prompt_bufnr)
-							-- TODO multiselect to insert all...?
-							vim.cmd(':normal i "' .. selection.value.name .. '"')
-						end)
-						return true
+			pickers.new(opts, {
+				finder = finders.new_table({
+					results = res.tags,
+					entry_maker = function(entry)
+						return {
+							display = entry.name .. " (" .. tostring(#entry.posts) .. ")",
+							ordinal = entry.name,
+							value = entry,
+						}
 					end,
-				})
-				:find()
+				}),
+				-- TODO sort tags by count?
+				sorter = config.generic_sorter(opts),
+				previewer = previewers.new_buffer_previewer({
+					title = "Tag detaiLs",
+					define_preview = function(self, entry)
+						local lines = {}
+						for _, post in ipairs(entry.value.posts) do
+							table.insert(lines, post.title)
+							table.insert(lines, post.url)
+							table.insert(lines, "")
+						end
+						vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, lines)
+					end,
+				}),
+				attach_mappings = function(prompt_bufnr)
+					actions.select_default:replace(function()
+						local selection = action_state.get_selected_entry()
+						actions.close(prompt_bufnr)
+						-- TODO multiselect to insert all...?
+						vim.cmd(':normal i "' .. selection.value.name .. '"')
+					end)
+					return true
+				end,
+			}):find()
 		end
 	end)
 end
 
-M.show_tags()
+M.show_posts = function()
+	nio.run(function()
+		local rg = nio.process.run({
+			cmd = "rg",
+			args = {
+				"-No",
+				-- "--json",
+				"title: (.+)",
+				blog_path .. "posts/",
+			},
+		})
+
+		local output = rg.stdout.read()
+		nio.scheduler()
+		local lines = vim.fn.split(output, "\n")
+		local posts = {}
+
+		local line_count = #lines
+		local i = 1
+		while i <= line_count do
+			i = i + 1
+		end
+
+		-- for _, line in ipairs(data) do
+		-- 	P(json)
+		-- 	if json.type == "match" then
+		-- 		table.insert(posts, {
+		-- 			path = json.data.path.text,
+		-- 		})
+		-- 	end
+		-- end
+
+		-- P(posts)
+		-- local res = vim.fn.json_decode(output)
+		-- P(res)
+	end)
+end
+
+M.show_posts()
 
 -- TODO these should just use fd and rg so we don't have to wait
 -- Browse posts
