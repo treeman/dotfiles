d6c7d1e0d4c9feb373f033fd242824554b83c649
Jonas Hietala <mail@jonashietala.se>
2024-05-20 08:33:09 +0200

    Use standard indent blankline settings

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,12 +1,1 @@
-local highlight = {
-  "CursorColumn",
-  "Whitespace",
-}
-require("ibl").setup({
-  indent = { highlight = highlight, char = "" },
-  whitespace = {
-    highlight = highlight,
-    remove_blankline_trail = false,
-  },
-  scope = { enabled = false },
-})
+require("ibl").setup({})
