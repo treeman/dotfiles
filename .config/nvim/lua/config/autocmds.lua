8a8934def68dcc7f498c0e48ec42633f812d07eb
Jonas Hietala <mail@jonashietala.se>
2024-05-24 06:07:47 +0200

    Remove lsp-rooter and use a custom rooter function

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -32,3 +32,12 @@ autocmd("WinLeave", {
     vim.opt_local.cursorline = false
   end,
 })
+
+autocmd("BufEnter", {
+  callback = function(ctx)
+    local root = vim.fs.root(ctx.buf, { ".git" })
+    if root then
+      vim.uv.chdir(root)
+    end
+  end,
+})
