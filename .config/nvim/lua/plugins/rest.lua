dcbcf1d47c5bda7004947dc6dbdd40d520270853
Jonas Hietala <mail@jonashietala.se>
2024-05-31 20:07:46 +0200

    Don't encode in rest.nvim

--- .config/nvim/lua/plugins/rest.lua
+++ .config/nvim/lua/plugins/rest.lua
@@ -1,9 +1,13 @@
 require("rest-nvim").setup({
+  encode_url = false,
   result = {
     split = {
       horizontal = true,
       in_place = false,
       stay_in_current_window_after_split = true,
     },
+  },
+  highliht = {
+    timeout = 200,
   },
 })
