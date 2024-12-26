0da233b6ee729d1357b13ea2cc666888259b3643
Jonas Hietala <mail@jonashietala.se>
2024-12-26 15:00:29 +0100

    WIP list type detection

--- .config/nvim/lua/org/lists.lua
+++ .config/nvim/lua/org/lists.lua
@@ -3,26 +3,30 @@ local ts = require("org.treesitter")
 local M = {}
 
 local function get_marker_type(list)
-  return list:child(0):child(0)
+  return list:child(0):child(0):type()
 end
 
 local function get_order_type(marker_type)
-  -- decimal
-  -- lower_alpha
-  -- upper_alpha
-  -- lower_roman
-  -- upper_roman
-  if string.find(marker_type, "decimal") then
-    return "decimal"
-  elseif string.find(marker_type, "lower_alpha") then
-    return "lower_alpha"
+  local order_types = { "decimal", "lower_alpha", "upper_alpha", "lower_roman", "upper_roman" }
+
+  for _, order_type in ipairs(order_types) do
+    if string.find(marker_type, order_type) then
+      return order_type
+    end
   end
 end
 
 function M.reset_list_numbering()
   local list = ts.find_node("list")
+  if not list then
+    return
+  end
+
   local marker_type = get_marker_type(list)
   local order_type = get_order_type(marker_type)
+  if not order_type then
+    return
+  end
 
   -- if list_type == "list_marker_decimal_period" then
   --
@@ -46,7 +50,7 @@ function M.reset_list_numbering()
   -- $.list_marker_upper_alpha_parens,
   -- $.list_marker_lower_roman_parens,
   -- $.list_marker_upper_roman_parens,
-  -- vim.notify("List type: " .. list_type)
+  vim.notify("Order type: " .. order_type)
 end
 
 return M
