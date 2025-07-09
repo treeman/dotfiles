997b6ec416080ce8507c18b9c7438e86ad45ba8e
Jonas Hietala <mail@jonashietala.se>
2025-07-09 20:49:51 +0200

    Workaround for text being pasted at startup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
 vim.loader.enable()
 
+-- Workaround for Neovim 0.11 inserting random characters upon start
+-- https://github.com/neovim/neovim/issues/33148#issuecomment-2815035441
+local _, vimtermcap = pcall(require, "vim.termcap")
+vimtermcap.query = function() end
+
 require("config.leader")
 require("config.lazy")
 require("config.options")
